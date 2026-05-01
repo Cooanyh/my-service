dda71a9f0cbe5ec8df0f8db20a0191459e0987cb
Cooanyh <yhexcek1538@outlook.com>
2026-05-01 21:47:55 +0800

    Remove Bilibili keepalive logic

--- TuneFlow/一键导出songs-json.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-
-start "" "%~dp001-myhkw-song-exporter.html"
-exit /b 0
--- TuneFlow/打开歌曲视频匹配GUI.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-
-start "" "%~dp002-song-video-matcher.html"
-exit /b 0
